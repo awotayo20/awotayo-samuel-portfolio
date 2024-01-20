1d65a02df9519191db9cd2b4b354f58a67f8018a
awotayo20 <50973905+awotayo20@users.noreply.github.com>
2024-01-20 17:22:06 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import LandingPage from './components/landingPage/landingPage.jsx'
 import WorkPage from './components/work/WorkPage.jsx'
 import AboutPage from './components/about/AboutPage.jsx'
-import Contact from './components/contact/ContactFrontPage.jsx'
 import ContactPage from './components/contact/ContactPage.jsx'
 
 const router = createBrowserRouter([
