6a79f17dede567a95883b444915ffa23eab68d62
awotayo20 <50973905+awotayo20@users.noreply.github.com>
2024-01-21 20:15:32 +0100

    rewrites ai

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import WorkPage from './components/work/WorkPage.jsx'
@@ -12,27 +8,27 @@ import AboutPage from './components/about/AboutPage.jsx'
 import ContactPage from './components/contact/ContactPage.jsx'
 import HomePage from './components/HomePage/HomePage.jsx'
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <HomePage />,
-//   },
-//   {
-//     path: '/work',
-//     element: <WorkPage />,
-//   },
-//   {
-//     path: '/about',
-//     element: <AboutPage />,
-//   },
-//   {
-//     path: '/contact',
-//     element: <ContactPage />,
-//   },
-// ])
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/work',
+    element: <WorkPage />,
+  },
+  {
+    path: '/about',
+    element: <AboutPage />,
+  },
+  {
+    path: '/contact',
+    element: <ContactPage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
