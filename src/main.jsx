92a1169a608fc26a12667e8501659f9a8457e4ed
awotayo20 <50973905+awotayo20@users.noreply.github.com>
2024-01-21 20:20:37 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import WorkPage from './components/work/WorkPage.jsx'
@@ -8,27 +12,8 @@ import AboutPage from './components/about/AboutPage.jsx'
 import ContactPage from './components/contact/ContactPage.jsx'
 import HomePage from './components/HomePage/HomePage.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/work',
-    element: <WorkPage />,
-  },
-  {
-    path: '/about',
-    element: <AboutPage />,
-  },
-  {
-    path: '/contact',
-    element: <ContactPage />,
-  },
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
